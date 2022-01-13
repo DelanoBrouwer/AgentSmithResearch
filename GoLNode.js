37e9bc2facb7509dec87d99a38ce8a0008c41f53
Delano Brouwer <43468859+DelanoBrouwer@users.noreply.github.com>
2022-01-13 14:29:30 +0100

    Update GoLNode.js

--- GoLNode.js
+++ GoLNode.js
@@ -20,22 +20,16 @@ const broker = new ServiceBroker({
     },
 });
 
-let stats = []
-
+// Creating a service with the "IsAlive" function that allows us to check whether a cell is alive or not.
 broker.createService({
     name: "Cell",
     actions: {
-        Echo(ctx) {
-            return ctx.params.message;
-        },
-
         isAlive(ctx) {
             let objects = ctx.params.objects;
             if (ctx.params.x >= 0) {
                 if (ctx.params.x < objects.length) {
                     if (ctx.params.y >= 0) {
                         if (ctx.params.y < objects[ctx.params.x].length) {
-                            //console.log(objects[ctx.params.x][ctx.params.y]);
                             stats.push(objects[ctx.params.x][ctx.params.y].alive);
                             return objects[ctx.params.x][ctx.params.y].alive ? 1 : 0;
                         }
