3687118c721dfe140e02da30f8eb05c6bd47da34
Delano Brouwer <43468859+DelanoBrouwer@users.noreply.github.com>
2022-01-17 14:44:04 +0100

    Comments update & Bugfix.
    Added missing action "Cell.Echo".

--- GoLNode.js
+++ GoLNode.js
@@ -2,6 +2,7 @@ const { ServiceBroker } = require("moleculer");
 
 const transporter = process.env.TRANSPORTER || "TCP";
 
+// Creating the servicebroker.
 const broker = new ServiceBroker({
     namespace: process.env.NAMESPACE || "nodes",
     nodeID: process.env.nodeID || "node-" + process.pid,
@@ -23,23 +24,24 @@ const broker = new ServiceBroker({
 broker.createService({
     name: "Cell",
     actions: {
+        // Returns the message. Was used for showing inter-node transport.
         Echo(ctx) {
             return ctx.params.message;
         },
 
+        // Checks whether a certain node is alive, and then returns either a 1 or a 0.
         isAlive(ctx) {
             let objects = ctx.params.objects;
             if (ctx.params.x >= 0) {
                 if (ctx.params.x < objects.length) {
                     if (ctx.params.y >= 0) {
                         if (ctx.params.y < objects[ctx.params.x].length) {
-                            //console.log(objects[ctx.params.x][ctx.params.y]);
                             return objects[ctx.params.x][ctx.params.y].alive ? 1 : 0;
                         }
                     }
                 }
             }
-            return false;
+            return 0;
 
         }
     }
